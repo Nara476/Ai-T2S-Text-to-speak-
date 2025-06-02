6ae14765498e912f80b405a95242b4e77a6171e7
Zeeshan Shrestha <shresthajeesan@gmail.com>
2025-06-02 22:09:08 +0545

    added the springboot backend for talking with python file

---
+++ SpringBootCodes/ai-t2s/src/main/java/com/ai_t2s/ai_t2s/AiT2sApplication.java
@@ -0,0 +1,13 @@
+package com.ai_t2s.ai_t2s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiT2sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiT2sApplication.class, args);
+	}
+
+}
---
+++ SpringBootCodes/ai-t2s/src/test/java/com/ai_t2s/ai_t2s/AiT2sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_t2s.ai_t2s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiT2sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
